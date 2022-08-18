222f3205ce12c0c16671ff94a2eea386f9fec8a5
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2022-08-18 15:50:09 +0900

    modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,73 @@
 require('dotenv').config();
 const env = process.env;
 import rpc from 'discord-rpc';
-import { createServer } from 'http';
+import { createServer, ServerResponse } from 'http';
 const client = new rpc.Client({ transport: 'ipc' })
 
 client.on("ready", () => {
-    client.setActivity({
-        state: "🤔",
-        details: "( ᐛ)",
-    });
+    client.setActivity(GetDefaultStatus());
 
-    const server = createServer((request, response) => {
-        response.writeHead(200, {
-            'Content-Type': 'application/json'
-        });
-    
+    const server = createServer((request, response) => {    
         request.on('data', (chunk: ArrayBuffer) => {
             const request_JSON = JSON.parse(chunk.toString());
 
-            if (request_JSON.closing == false) {
+            if (request_JSON.closing == false && request_JSON.AppName != undefined) {
                 client.setActivity({
                     state: request_JSON.AppName,
                     details: `${request_JSON.AppName}をプレイ中`,
                     smallImageKey: GetAppShortName(request_JSON.AppName),
                     startTimestamp: new Date(),
-                });
+                }).catch(((error : Error)=>{
+                    ErrorResponce(response);
+                    return;
+                }));
+
                 console.log(`Set RPC to : ${request_JSON.AppName}`);
+
+                response.writeHead(200, {
+                    'Content-Type': 'application/json'
+                });
+
+                const responseMessage = { 'message': `Set RPC to : ${request_JSON.AppName}` };
+                response.end(JSON.stringify(responseMessage));
             }
             else{
-                client.setActivity({
-                    state: "🤔",
-                    details: "( ᐛ)",
+                client.setActivity(GetDefaultStatus()).catch((error : Error)=>{
+                    ErrorResponce(response);
+                    return;
                 });
+
+                response.writeHead(200, {
+                    'Content-Type': 'application/json'
+                });
+
+                const responseMessage = { 'message': 'Set to default' };
+                response.end(JSON.stringify(responseMessage));
             }
         });
-    
-        const responseMessage = { 'message': 'OK' };
-        response.end(JSON.stringify(responseMessage));
     });
 
     server.listen(8080);
 });
+
+function ErrorResponce(response : ServerResponse){
+    response.writeHead(201, {
+        'Content-Type': 'application/json'
+    });
+
+    const responseMessage = { 'message': 'Error occured' };
+    response.end(JSON.stringify(responseMessage));
+}
+
+
+function GetDefaultStatus(){
+    const rpcstatus : rpc.Presence = {
+        state: "🤔",
+        details: "( ᐛ)",
+    }
+
+    return rpcstatus;
+}
 
 function GetAppShortName(appname : string){
     let AppName = '';
